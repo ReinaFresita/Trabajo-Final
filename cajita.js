ea7d5e552977261ee49d60b76350051d78c38971
ReinaFresita <micaladelfina@gmail.com>
2022-07-28 18:31:02 -0500

    subiendo archivos

---
+++ cajita.js
@@ -0,0 +1,7 @@
+alert("Miniencuesta para ayudar a que mejore el sitio!");
+
+var names = prompt("¿Cuál es tu nombre?: ");
+console.log(names)
+var age = prompt("¿Cuál es tu edad?: ");
+console.log(age);
+alert("Gracias por responder!");
